8cb77e85709d18451637f733a073356ffe8d10cd
Vladimir Logunov <logunov@mybook.ru>
2022-08-16 13:19:02 +0300

    Remove Signal providing_args for Django 4.x support

--- atol/signals.py
+++ atol/signals.py
@@ -16,6 +16,6 @@ receipt_received:
 """
 from django.dispatch import Signal
 
-receipt_initiated = Signal(providing_args=['receipt'])
-receipt_failed = Signal(providing_args=['receipt'])
-receipt_received = Signal(providing_args=['receipt'])
+receipt_initiated = Signal()
+receipt_failed = Signal()
+receipt_received = Signal()
